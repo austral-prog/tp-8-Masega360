1af1c3e40d24cd8edd99186933e189593b5bc319
Tomas <117113921+Masega360@users.noreply.github.com>
2024-05-24 11:48:21 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,19 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
+def get_coordinate(tup):
+    tupli = tup[1]
+    return tupli
 
+def convert_coordinate(coordenada):
+    v1 = coordenada[0:1]
+    v2 = coordenada[1:]
+    coord = v1, v2
+    return coord
 
-def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
-
-def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
-
-
-def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+def create_record(tup1, tup2):
+    coordenada = tup1[1]
+    v1 = coordenada[0:1]
+    v2 = coordenada[1:2]
+    coord = v1, v2
+    if coord == tup2[1]:
+        return tup1 + tup2
+    else:
+        return "no coincide"
